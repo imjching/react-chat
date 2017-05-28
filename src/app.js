2b0ae1fff8c03f625b0630d4c11b974bee31b1a0
Jay <imjching@users.noreply.github.com>
2017-05-28 14:23:46 -0700

    init files with webpack 2.0

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<p>React lives!</p>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/assets/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+    publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react'],
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
